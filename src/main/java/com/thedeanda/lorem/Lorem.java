1d59d9fd294c3d7bca0728b7add4f600f070ca89
miguel de anda <miguel@thedeanda.com>
2014-10-22 00:11:58 -0700

    removed all static methods. made it too difficult to support alternative "lorems" in the future. moving towards gradle builds instead

--- src/main/java/com/thedeanda/lorem/Lorem.java
+++ src/main/java/com/thedeanda/lorem/Lorem.java
@@ -10,7 +10,6 @@ import java.util.Random;
 import java.util.Set;
 
 public class Lorem {
-	private static Lorem instance = new Lorem();
 	private List<String> words = new ArrayList<String>();
 	private Random random = new Random();
 	private List<String> maleNames;
@@ -20,75 +19,7 @@ public class Lorem {
 	private List<String> stateAbbr;
 	private List<String> stateFull;
 
-	public static String getPhone() {
-		return instance._getPhone();
-	}
-
-	public static String getFirstName() {
-		return instance.getRandom(instance.firstNames);
-	}
-
-	public static String getFirstNameMale() {
-		return instance.getRandom(instance.maleNames);
-	}
-
-	public static String getFirstNameFemale() {
-		return instance.getRandom(instance.femaleNames);
-	}
-
-	public static String getLastName() {
-		return instance.getRandom(instance.surnames);
-	}
-
-	public static String getName() {
-		return getFirstName() + " " + getLastName();
-	}
-
-	public static String getNameMale() {
-		return getFirstNameMale() + " " + getLastName();
-	}
-
-	public static String getNameFemale() {
-		return getFirstNameFemale() + " " + getLastName();
-	}
-
-	public static String getTitle(int count) {
-		return instance._getWords(count, count, true);
-	}
-
-	public static String getTitle(int min, int max) {
-		return instance._getWords(min, max, true);
-	}
-
-	public static String getWords(int count) {
-		return instance._getWords(count, count, false);
-	}
-
-	public static String getWords(int min, int max) {
-		return instance._getWords(min, max, false);
-	}
-
-	public static String getHtmlParagraphs(int min, int max) {
-		return instance._getHtmlParagraphs(min, max);
-	}
-
-	public static String getParagraphs(int min, int max) {
-		return instance._getParagraphs(min, max);
-	}
-
-	public static String getStateAbbr() {
-		return instance._getStateAbbr();
-	}
-
-	public static String getStateFull() {
-		return instance._getStateFull();
-	}
-
-	public static String getZipCode() {
-		return instance._getZipCode();
-	}
-
-	private Lorem() {
+	public Lorem() {
 		readLorem();
 		maleNames = readLines("male_names.txt");
 		femaleNames = readLines("female_names.txt");
@@ -168,29 +99,29 @@ public class Lorem {
 		return count;
 	}
 
-	private String _getHtmlParagraphs(int min, int max) {
+	public String getHtmlParagraphs(int min, int max) {
 		int count = getCount(min, max);
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < count; i++) {
 			sb.append("<p>");
-			sb.append(_getParagraphs(1, 1));
+			sb.append(getParagraphs(1, 1));
 			sb.append("</p>");
 		}
 		return sb.toString().trim();
 	}
 
-	private String _getParagraphs(int min, int max) {
+	public String getParagraphs(int min, int max) {
 		int count = getCount(min, max);
 		StringBuilder sb = new StringBuilder();
 		for (int j = 0; j < count; j++) {
 			int sentences = random.nextInt(5) + 2; // 2 to 6
 			for (int i = 0; i < sentences; i++) {
-				String first = _getWords(1, 1, false);
+				String first = getWords(1, 1, false);
 				first = first.substring(0, 1).toUpperCase()
 						+ first.substring(1);
 				sb.append(first);
 
-				sb.append(_getWords(2, 20, false));
+				sb.append(getWords(2, 20, false));
 				sb.append(".  ");
 			}
 			sb.append("\n");
@@ -198,12 +129,12 @@ public class Lorem {
 		return sb.toString().trim();
 	}
 
-	private String _getWords(int min, int max, boolean title) {
+	public String getWords(int min, int max, boolean title) {
 		int count = getCount(min, max);
-		return instance._getWords(count, title);
+		return getWords(count, title);
 	}
 
-	private String _getWords(int count, boolean title) {
+	public String getWords(int count, boolean title) {
 		StringBuilder sb = new StringBuilder();
 		int size = words.size();
 		int wordCount = 0;
@@ -227,7 +158,7 @@ public class Lorem {
 		return list.get(random.nextInt(size));
 	}
 
-	private String _getPhone() {
+	public String getPhone() {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("(");
@@ -249,15 +180,15 @@ public class Lorem {
 		return sb.toString();
 	}
 
-	private String _getStateAbbr() {
+	public String getStateAbbr() {
 		return getRandom(stateAbbr);
 	}
 
-	private String _getStateFull() {
+	public String getStateFull() {
 		return getRandom(stateFull);
 	}
 
-	private String _getZipCode() {
+	public String getZipCode() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 5; i++) {
 			sb.append(random.nextInt(10));
