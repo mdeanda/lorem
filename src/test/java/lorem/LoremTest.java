80a8494c0260456e663eefaf4447507df347d0fe
miguel de anda <miguel@thedeanda.com>
2015-10-20 21:13:48 -0700

    small updates

--- src/test/java/lorem/LoremTest.java
+++ src/test/java/lorem/LoremTest.java
@@ -17,7 +17,7 @@ public class LoremTest {
 		lorem = LoremIpsum.getInstance();
 	}
 
-	private void testBlank(String s) {
+	private void notBlank(String s) {
 		assertNotNull(s);
 		assertNotEquals("", s.trim());
 	}
@@ -25,30 +25,51 @@ public class LoremTest {
 	@Test
 	public void testCity() {
 		String s = lorem.getCity();
-		testBlank(s);
+		notBlank(s);
 	}
 
 	@Test
 	public void testCountry() {
 		String s = lorem.getCountry();
-		testBlank(s);
+		notBlank(s);
 	}
 
 	@Test
 	public void testEmail() {
 		String s = lorem.getEmail();
-		testBlank(s);
+		notBlank(s);
+	}
+
+	@Test
+	public void testNameMale() {
+		String s = lorem.getNameMale();
+		notBlank(s);
+	}
+
+	@Test
+	public void testNameFemale() {
+		String s = lorem.getNameFemale();
+		notBlank(s);
 	}
 
 	@Test
 	public void testParagraphs() {
 		String p1 = lorem.getParagraphs(3, 5);
-		testBlank(p1);
+		notBlank(p1);
 
 		String p2 = lorem.getParagraphs(6, 8);
-		testBlank(p2);
+		notBlank(p2);
 
 		assertNotEquals(p1, p2);
+	}
+
+	@Test
+	public void testState() {
+		String s = lorem.getStateAbbr();
+		notBlank(s);
+
+		s = lorem.getStateFull();
+		notBlank(s);
 	}
 
 	@Test
@@ -63,4 +84,11 @@ public class LoremTest {
 
 		assertNotEquals(words0, words1);
 	}
+
+	@Test
+	public void testZip() {
+		String s = lorem.getZipCode();
+		notBlank(s);
+	}
+
 }
